5646b89f860ea5960d16d2c51d98c4c6391106f9
Muhammad Rizal Wiyono <69446968+RizalWiyono@users.noreply.github.com>
2021-12-13 09:31:44 +0700

    Fix code

--- database/db_crawling.sql
+++ database/db_crawling.sql
@@ -34,7 +34,7 @@ CREATE TABLE `tb_tweets` (
   `tweets` varchar(100) NOT NULL,
   `overlap` varchar(20) NOT NULL,
   `asymmetric` varchar(20) NOT NULL,
-  `jaccard` varchar(20) NOT NULL
+  `cosine` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
